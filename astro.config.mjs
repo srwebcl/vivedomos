96ef7b5d1b966beef09952b8fafadf0ba703f4dd
srwebcl <contacto@srweb.cl>
2026-06-08 12:19:29 -0400

    Fix Vercel Build (Sitemap Bug & CSS Refs)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
     site: 'https://vivedomos.cl',
     output: 'hybrid',
     adapter: vercel(),
-    integrations: [sitemap()],
 });
